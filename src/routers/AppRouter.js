612530f021efba6f05cdfa21c2c3abc12fe7ca83
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-11-12 17:53:11 -0500

    Finishing up

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, Switch} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
-import DashboardPage from '../components/FoodTruckDashboard';
+import DashboardPage from '../components/Dashboard';
 
 export const history = createHistory();
 
